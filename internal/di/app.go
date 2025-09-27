3a735f604f4ab4fc41d9fc4f17e0ff051fc52f09
Marcelo Fabiano <marcelofabianov@gmail.com>
2025-09-27 13:43:29 -0300

    feat: criar DI com fx

--- internal/di/app.go
+++ internal/di/app.go
@@ -1,1 +1,45 @@
 package di
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"go.uber.org/fx"
+
+	"github.com/marcelofabianov/weather-api/internal/handler"
+)
+
+func NewApp() *fx.App {
+	return fx.New(
+		AppModule,
+		fx.Invoke(registerHooks),
+	)
+}
+
+func registerHooks(
+	lifecycle fx.Lifecycle,
+	logger *slog.Logger,
+	handler *handler.WeatherHandler,
+	router *chi.Mux,
+	server *http.Server,
+) {
+	handler.RegisterRoutes(router)
+
+	lifecycle.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			logger.Info("Starting server", "address", server.Addr)
+			go func() {
+				if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+					logger.Error("failed to start server", "error", err)
+				}
+			}()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			logger.Info("Stopping server")
+			return server.Shutdown(ctx)
+		},
+	})
+}
--- internal/di/module.go
+++ internal/di/module.go
@@ -1,1 +1,121 @@
 package di
+
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/sony/gobreaker"
+	"go.uber.org/fx"
+
+	"github.com/marcelofabianov/weather-api/config"
+	"github.com/marcelofabianov/weather-api/internal/adapter"
+	"github.com/marcelofabianov/weather-api/internal/handler"
+	"github.com/marcelofabianov/weather-api/internal/port"
+	"github.com/marcelofabianov/weather-api/internal/service"
+	"github.com/marcelofabianov/weather-api/pkg/logger"
+	"github.com/marcelofabianov/weather-api/pkg/web"
+)
+
+var AppModule = fx.Options(
+	ConfigModule,
+	LoggerModule,
+	AdaptersModule,
+	CoreModule,
+	HandlersModule,
+	WebServerModule,
+)
+
+var ConfigModule = fx.Module("config",
+	fx.Provide(
+		func() (*config.Config, error) {
+			return config.LoadConfig(".")
+		},
+		func(cfg *config.Config) *config.LoggerConfig { return &cfg.Logger },
+		func(cfg *config.Config) *config.ServerConfig { return &cfg.Server },
+		func(cfg *config.Config) *config.ResilienceConfig { return &cfg.Resilience },
+		func(cfg *config.Config) *config.ClientsConfig { return &cfg.Clients },
+		func(clientsCfg *config.ClientsConfig) *config.WeatherAPIConfig { return &clientsCfg.WeatherAPI },
+	),
+)
+
+var LoggerModule = fx.Module("logger",
+	fx.Provide(func(cfg *config.LoggerConfig) *slog.Logger {
+		return logger.NewSlogLogger(cfg)
+	}),
+)
+
+var AdaptersModule = fx.Module("adapters",
+	fx.Provide(
+		fx.Annotate(
+			func(cfg *config.ResilienceConfig) *gobreaker.CircuitBreaker {
+				st := gobreaker.Settings{
+					Name:        "ViaCEP",
+					MaxRequests: 3,
+					Interval:    0,
+					Timeout:     cfg.BreakerTimeout,
+					ReadyToTrip: func(counts gobreaker.Counts) bool {
+						return counts.ConsecutiveFailures > cfg.BreakerMaxFailures
+					},
+				}
+				return gobreaker.NewCircuitBreaker(st)
+			},
+			fx.ResultTags(`name:"viaCepBreaker"`),
+		),
+		fx.Annotate(
+			func(cfg *config.ResilienceConfig) *gobreaker.CircuitBreaker {
+				st := gobreaker.Settings{
+					Name:        "WeatherAPI",
+					MaxRequests: 3,
+					Interval:    0,
+					Timeout:     cfg.BreakerTimeout,
+					ReadyToTrip: func(counts gobreaker.Counts) bool {
+						return counts.ConsecutiveFailures > cfg.BreakerMaxFailures
+					},
+				}
+				return gobreaker.NewCircuitBreaker(st)
+			},
+			fx.ResultTags(`name:"weatherApiBreaker"`),
+		),
+	),
+	fx.Provide(
+		fx.Annotate(
+			func(breaker *gobreaker.CircuitBreaker, resilienceCfg *config.ResilienceConfig, logger *slog.Logger) *adapter.ViaCepClient {
+				return adapter.NewViaCepClient(breaker, resilienceCfg, logger)
+			},
+			fx.ParamTags(`name:"viaCepBreaker"`),
+			fx.As(new(port.ViaCepClient)),
+		),
+		fx.Annotate(
+			func(breaker *gobreaker.CircuitBreaker, clientCfg *config.WeatherAPIConfig, resilienceCfg *config.ResilienceConfig, logger *slog.Logger) *adapter.WeatherApiClient {
+				return adapter.NewWeatherApiClient(clientCfg, breaker, resilienceCfg, logger)
+			},
+			fx.ParamTags(`name:"weatherApiBreaker"`),
+			fx.As(new(port.WeatherApiClient)),
+		),
+	),
+)
+
+var CoreModule = fx.Module("core",
+	fx.Provide(
+		fx.Annotate(
+			service.NewWeatherService,
+			fx.As(new(port.WeatherService)),
+		),
+	),
+)
+
+var HandlersModule = fx.Module("handlers",
+	fx.Provide(handler.NewWeatherHandler),
+)
+
+var WebServerModule = fx.Module("webserver",
+	fx.Provide(
+		func(cfg *config.ServerConfig, logger *slog.Logger) *chi.Mux {
+			return web.NewRouter(cfg, logger)
+		},
+		func(cfg *config.Config, logger *slog.Logger, router *chi.Mux) *http.Server {
+			return web.NewServer(cfg, logger, router)
+		},
+	),
+)
